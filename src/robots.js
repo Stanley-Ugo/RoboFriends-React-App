edfd00a7d57d292ac3e8da83f035a95cea62d4b4
Stanley-Ugo <iamKosmicblast@gmail.com>
2020-07-12 00:28:43 +0100

    initial commit

---
+++ src/robots.js
@@ -0,0 +1,62 @@
+export const robots = [
+    {
+        id: 1,
+        name: 'Leanne Graham',
+        username: 'Bret',
+        email: 'Sincere@april.biz'
+    },
+    {
+        id: 2,
+        name: 'Erwin Howell',
+        username: 'Antoinette',
+        email: 'Shanna@melisa.tv'
+    },
+    {
+        id: 3,
+        name: 'Mitchell Scotch',
+        username: 'Summer',
+        email: 'Lemae@fronfix.biz'
+    },
+    {
+        id: 4,
+        name: 'Clementina Baruch',
+        username: 'Samantha',
+        email: 'Samantapain@gmail.biz'
+    },
+    {
+        id: 5,
+        name: 'Prat leibnitz',
+        username: 'Carne',
+        email: 'comatose@dawn.dig'
+    },
+    {
+        id: 6,
+        name: 'Leanne Graham',
+        username: 'Bret',
+        email: 'Sincere@april.biz'
+    },
+    {
+        id: 7,
+        name: 'Leanne Graham',
+        username: 'Bret',
+        email: 'Sincere@april.biz'
+    },
+    {
+        id: 8,
+        name: 'Patricia Liberace',
+        username: 'Katrina',
+        email: 'Kattypowel@powpow.hitz'
+    },
+    {
+        id: 9,
+        name: 'Nina Gluck',
+        username: 'Berretta',
+        email: 'Split@dome.bis'
+    },
+    {
+        id: 10,
+        name: 'Chelsea Dietrich',
+        username: 'Luella',
+        email: 'Bumclap@strip.tiz'
+    },
+ ]

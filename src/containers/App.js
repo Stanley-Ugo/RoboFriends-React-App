0a44ccc0b5a59bc26ea4bab47e9ba50f330ae245
Stanley-Ugo <iamKosmicblast@gmail.com>
2020-07-12 19:44:48 +0100

    added a redux connect()(App) for state and dispatch management

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,17 +1,31 @@
 import React, { Component } from "react";
+import { connect } from 'react-redux';
 import CardList from "../components/CardList";
 import SearchBox from "../components/SearchBox";
 import Scroll from '../components/Scroll';
 import ErrorBoundary from '../components/ErrorBoundary';
 import './App.css';
 
+import { setSearchField } from '../actions';
+
+const mapStateToProps = state => {
+  return {
+    searchField: state.searchField
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    onSearchChange : (event) => dispatch(setSearchField(event.target.value))
+  }
+}
+
 class App extends Component {
   // eslint-disable-next-line no-useless-constructor
   constructor() {
     super();
     this.state = {
-      robots: [],
-      searchfield: "",
+      robots: []
     };
   }
 
@@ -21,22 +35,20 @@ class App extends Component {
     .then(users => {this.setState({robots: users})});
   }
 
-  onSearchChange = (event) => {
-    this.setState({ searchfield: event.target.value });
-  };
   render() {
-    const { robots, searchfield } = this.state;
+    const { robots } = this.state;
+    const { searchField, onSearchChange } = this.props;
     const filteredRobots = robots.filter((robots) => {
       return robots.name
         .toLowerCase()
-        .includes(searchfield.toLowerCase());
+        .includes(searchField.toLowerCase());
     });
     return !robots.length ? 
       <h1>Loading</h1> :
       (
         <div className="tc">
           <h1 className="f1">RobotFriends</h1>
-          <SearchBox searchChange={this.onSearchChange} />
+          <SearchBox searchChange={ onSearchChange } />
           <Scroll>
             <ErrorBoundary>
               <CardList robots={filteredRobots} />
@@ -47,4 +59,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default connect(mapStateToProps, mapDispatchToProps)(App);
